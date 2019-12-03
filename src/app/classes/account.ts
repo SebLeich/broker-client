fcb11f3575485b3454374f69c3c98e775a0ae9a6
Marco <marcogercken@hotmail.com>
2019-12-03 12:03:55 +0100

    Registration works fine

--- src/app/classes/account.ts
+++ src/app/classes/account.ts
@@ -1,80 +1,112 @@
+import {
+  FormGroup,
+  FormControl,
+  ValidatorFn,
+  AbstractControl
+} from "@angular/forms";
+
 export class RoleRight {
+  public isAllowed: boolean;
+  public roles: Role[];
+  public rule: Rule;
+  public ruleId: number;
 
-    public isAllowed: boolean;
-    public roles: Role[];
-    public rule: Rule;
-    public ruleId: number;
-
-    constructor(object){
-        this.isAllowed = object.isAllowed;
-        this.roles = object.roles;
-        this.rule = new Rule(object.rule);
-        this.ruleId = object.ruleId;
-    }
-
+  constructor(object) {
+    this.isAllowed = object.isAllowed;
+    this.roles = object.roles;
+    this.rule = new Rule(object.rule);
+    this.ruleId = object.ruleId;
+  }
 }
 
 export class Role {
+  public roleName: string;
 
-    public roleName: string;
-
-    constructor(object){
-        this.roleName = object.roleName;
-    }
+  constructor(object) {
+    this.roleName = object.roleName;
+  }
 }
 
 export class Rule {
+  public id: number;
+  public ruleCode: string;
+  public ruleDesc: string;
+  public ruleTitle: string;
 
-    public id: number;
-    public ruleCode: string;
-    public ruleDesc: string;
-    public ruleTitle: string;
-
-    constructor(object){
-        this.id = object.id;
-        this.ruleCode = object.ruleCode;
-        this.ruleDesc = object.ruleDesc;
-        this.ruleTitle = object.ruleTitle;
-    }
-    
+  constructor(object) {
+    this.id = object.id;
+    this.ruleCode = object.ruleCode;
+    this.ruleDesc = object.ruleDesc;
+    this.ruleTitle = object.ruleTitle;
+  }
 }
 
 /**
  * the class contains an application's user
  */
 export class User {
-    public id: string;
-    public username: string;
-    public password: string;
-    public confirmPassword: string;
-    public roles: string[] = [];
-    /**
-     * the constructor creates a new instance of an user
-     */
-    constructor(object?){
-        if(object != null && typeof(object) != "undefined"){
-            this.id = object.id;
-            this.username = object.userName;
-            this.password = object.password;
-            this.confirmPassword = object.confirmPassword;
-            for(var index in object.roles) this.roles.push(object.roles[index].roleName);
-        }
+  public id: string;
+  public username: string;
+  public password: string;
+  public confirmPassword: string;
+  public roles: string[] = [];
+  /**
+         * 1 uppercase letter
+          1 lowercase letter
+          A number
+          A minimum length of 8.
+         */
+  public passWordrequirements =
+    "^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\\D*\\d)[A-Za-z\\d!$%@#£€*?&]{8,}$";
+  public minPasswordlenght = 8;
+  public minUsernamelenght = 4;
+  /**
+   * the constructor creates a new instance of an user
+   */
+  constructor(object?) {
+    if (object != null && typeof object != "undefined") {
+      this.id = object.id;
+      this.username = object.userName;
+      this.password = object.password;
+      this.confirmPassword = object.confirmPassword;
+      for (var index in object.roles)
+        this.roles.push(object.roles[index].roleName);
     }
-    /**
-     * the method checks whether the user model is valid for registration
-     */
-    isLoginValid() {
-        if (this.username.length < 4) return false;
-        if (this.password.length < 6) return false;
-        return true;
+  }
+  /**
+   * the method checks whether the user model is valid for registration
+   */
+  isLoginValid() {
+    if (this.username.length < this.minUsernamelenght) return false;
+    if (this.password.length < this.minPasswordlenght) return false;
+    return true;
+  }
+  /**
+   * the method checks whether the user model is valid for registration
+   */
+  isRegistrationValid() {
+    if (this.username.length < this.minUsernamelenght) return false;
+    if (this.password.length < this.minPasswordlenght) return false;
+    if (this.password != this.confirmPassword) return false;
+    return true;
+  }
+
+  equalPasswords(myForm: FormGroup): boolean {
+    const password1 = myForm.get("password1").value;
+    const password2 = myForm.get("password2").value;
+
+    const matched: boolean = password1 === password2;
+
+    console.log("equaltest", matched);
+
+    if (matched) {
+      myForm.controls.password2.setErrors(null);
+    } else {
+      myForm.controls.password2.setErrors({
+        notMatched: true
+      });
     }
-    /**
-     * the method checks whether the user model is valid for registration
-     */
-    isRegistrationValid() {
-        if (this.username.length < 4) return false;
-        if (this.password.length < 6) return false;
-        if (this.password != this.confirmPassword) return false;
-        return true;
-    }
+
+    return matched;
+  }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject, Output, EventEmitter } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import * as globals from "../../globals";
 import { User } from "../../classes/account";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-register",
@@ -18,6 +19,21 @@ export class RegisterComponent implements OnInit {
    */
   @Output() submitData = new EventEmitter<any>();
 
+  /**
+   * the FormGroup ist required for the validation
+   */
+  mySignupForm: FormGroup;
+
+  get password1() {
+    return this.mySignupForm.get("password1");
+  }
+  get password2() {
+    return this.mySignupForm.get("password2");
+  }
+  get username() {
+    return this.mySignupForm.get("username");
+  }
+
   constructor(
     /**
      * the data injected by the root component
@@ -27,9 +43,7 @@ export class RegisterComponent implements OnInit {
      * the dialog parent
      */
     private dialogRef: MatDialogRef<RegisterComponent>
-  ) {
-
-  }
+  ) {}
   /**
    * the method emits the user credentials
    */
@@ -40,8 +54,22 @@ export class RegisterComponent implements OnInit {
    * the method is called on component initalization
    */
   ngOnInit() {
-
+    this.mySignupForm = new FormGroup({
+      username: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(15)
+      ]),
+      password1: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(15),
+        Validators.pattern(this.user.passWordrequirements)
+      ]),
+      password2: new FormControl(null, Validators.required)
+    });
   }
+
   /**
    * the method sets the component states
    */
